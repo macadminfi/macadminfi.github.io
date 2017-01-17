23a4dfaffdad7446af84f27d835fbce349f12561
Pettinen Antti <antti.pettinen@tut.fi>
2017-01-17 16:02:49 +0200

    removed duplicates

--- deploy.sh
+++ deploy.sh
@@ -165,7 +165,7 @@ commit+push() {
 	disable_expanded_output
 	#--quiet is important here to avoid outputting the repo URL, which may contain a secret token
 	git push --quiet $repo $deploy_branch
-  git push --quiet -u origin master
+
 	enable_expanded_output
 }
 
